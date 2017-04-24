4b080d86d1248125f32496751547e5407de7de8b
Jady <taiyi.pei@gmail.com>
2017-04-24 15:11:12 +0800

    First commit, hello world

---
+++ 2-9/hello.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+main()
+{
+	string name;
+	int age = 0;
+	
+	name = "Jady";
+	age = 31;
+  cout << "My name is: " << name << ", age: " << age << endl; 
+  
+}
