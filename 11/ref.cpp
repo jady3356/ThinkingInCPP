5d1f4a18b1a3650e8ec970d369596b596204d1ee
Jady <taiyi.pei@gmail.com>
2017-12-06 15:55:50 +0800

    添加引用的简单例子和简单的成员指针用法

--- 11/ref.cpp
+++ 11/ref.cpp
@@ -15,7 +15,7 @@ struct Big {
   char buf[100];
   int i;
   long d;
-} B, B2; //定义了1个数据对象，B2？
+} B, B2; //定义了名字为Big的结构，同时申明了2个数据对象，B2和B
 
 Big bigfun(Big b) {
   b.i = 100; // Do something to the argument
@@ -71,40 +71,44 @@ HowMany f(HowMany x) {
 
 
 int main() {
-	int x = 1;
-	int& a = x;
+  /*    引用
+    (1) 定义：为一个变量（目标）设置别名，对引用的操作与对变量的操作一样;申明时必须同时初始化
+    (2) 适用场景：1.作为函数的入参或返回值，会提高效率，因为直接对实参操作，不需要拷贝行参为其分配内存副本，有点类似指针，但是指针也是形参，需要拷贝副本；
+                 2.常（const）引用，函数入参是引用时尽量使用const；
+                 3.引用作为函数返回值，好处是内存中不会产生返回值的副本。注意：不能返回局部变量的引用；不能返回new出来的mem的引用。
+  */
+  int x = 1;
+	int& a = x;// a是x的引用，引用就是a和x的绑定，a是x的别名
 	a++;
-	//cout <<　"a address = " << &a <<　endl;
 	cout << "x address= " << &x << endl;
 	cout << "a = "<< a << " x = " << x << endl; 
 
-	int* i = &a;//指针引用
-	
-    cout << "i = " << i << endl;
-    increment(i);
-    cout << "i = " << i << endl;
+	int* i = &a;//指针引用,用p对引用a取地址
+  cout << "i = " << i << endl;//a是x的引用，所以他们的地址一样
+  increment(i);
+  cout << "i = " << i << endl;
 
-	B2 = bigfun(B);//B代表一种数据类型
-	cout << "B2.i= " << B2.i << " B.i= " << B.i << endl; 
+	B2 = bigfun(B);//B是一个Big的数据类型
+	cout << "B2.i= " << B2.i << " B.i= " << B.i << endl; //这里的入参B.i不会被改写？
 
 	Data d, *dp = &d;
-    int Data::*pmInt = &Data::a;
-    dp->*pmInt = 47;
-    pmInt = &Data::b;
-    d.*pmInt = 48;
-    pmInt = &Data::c;
-    dp->*pmInt = 49;
-    dp->print();
+  int Data::*pmInt = &Data::a;//定义整型的成员指针pm，并让其指向类成员a的地址
+  dp->*pmInt = 47; 
+  pmInt = &Data::b;//pm重新指向b成员
+  d.*pmInt = 48;
+  pmInt = &Data::c;//pm重新指向c成员
+  dp->*pmInt = 49;
+  dp->print();
 
-    A aa;
-    aa.print();
-    aa.add();
-    aa.print();
-    A bb;//静态成员所有对象共有
-    bb.print();
+  A aa;
+  aa.print();
+  aa.add();
+  aa.print();
+  A bb;//静态成员所有对象共有
+  bb.print();
 
-    HowMany h;
-    HowMany::print("after construction of h");
-    HowMany h2 = f(h); //就类对象而言，相同类型的类对象是通过拷贝构造函数来完成整个复制过程的
-    HowMany::print("after call to f()");
+  HowMany h;
+  HowMany::print("after construction of h");
+  HowMany h2 = f(h); //就类对象而言，相同类型的类对象是通过拷贝构造函数来完成整个复制过程的
+  HowMany::print("after call to f()");
 }
